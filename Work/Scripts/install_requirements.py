bcdadc5932b44631cff8feb8bc2614d17f505c41
Мацнев Владимир Дмитриевич <vdmatsnev@edu.hse.ru>
2024-06-12 22:49:41 +0300

    font installation

--- Work/Scripts/install_requirements.py
+++ Work/Scripts/install_requirements.py
@@ -1,10 +1,13 @@
 """
 Скрипт для автоматической установки зависимостей путем запуска подпроцессов
+и установки шрифта для приложения с помощью менеджера шрифтов matplotlib
 """
 
 import subprocess
+import os
+import matplotlib.font_manager as fm
 
-
+# Установка зависимостей
 try:
     with open('requirements.txt', 'r') as file:
         dependencies = file.read().splitlines()
@@ -18,4 +21,27 @@ except FileNotFoundError:
     print("requirements.txt не найден.")
 except subprocess.CalledProcessError as e:
     print(f"Ошибка при установке зависимостей: {e}")
+
+# Установка шрифта
+try:
+    # Получение пути к директории шрифтов системы
+    font_file = "HYWenHei.ttf"
+    font_dir = fm.get_home()
+
+    # Копирование файла шрифта в директорию шрифтов
+    font_path = os.path.join(font_dir, font_file)
+    with open(font_file, 'rb') as source_file:
+        with open(font_path, 'wb') as dest_file:
+            dest_file.write(source_file.read())
+
+    # Перестройка кэша шрифтов
+    fm._rebuild()
+
+    print(f"Шрифт установлен успешно!")
+except FileNotFoundError:
+    print(f"Файл шрифта не найден.")
+except PermissionError:
+    print(f"Доступ запрещен. Установка шрифта не была завершена.")
+except Exception as e:
+    print(f"Ошибка при установке шрифта: {e}")
 
